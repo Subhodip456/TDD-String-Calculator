53aee807ffa9a95856d36bfc1822dfc0951ff9f1
Subhodip456 <53405787+Subhodip456@users.noreply.github.com>
2024-11-07 21:07:25 +0530

    Added new changes that supports negative numbers  in string and throws exception
    Calling add with a negative number will throw an exception: "negative numbers not allowed <negative_number>".
    
    If there are multiple negative numbers, show all of them in the exception message, separated by commas.
    
    Signed-off-by: Subhodip456 <53405787+Subhodip456@users.noreply.github.com>

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,17 +1,43 @@
 function add(numbers) {
+  if (!numbers) return 0;
+
   let delimiter = /,|\n/; 
   if (numbers.startsWith("//")) {
-    const delimiterEnd = numbers.indexOf("\n"); 
-    delimiter = new RegExp(numbers.substring(2, delimiterEnd)); 
-    numbers = numbers.substring(delimiterEnd + 1); 
+    const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (delimiterMatch) {
+      delimiter = new RegExp(delimiterMatch[1]);
+      numbers = numbers.slice(delimiterMatch[0].length);
+    }
   }
-
-  const numbersArray = numbers.split(delimiter); 
-  const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0); 
-  return parsedNumbers.reduce((sum, num) => sum + num, 0); 
+  const numbersArray = numbers.split(delimiter);
+  const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0);
+  const negativeNumbers = parsedNumbers.filter(num => num < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negativeNumbers.join(", ")}`);
+  }
+  return parsedNumbers.reduce((sum, num) => sum + num, 0);
 }
 
-// Test cases
-console.log(add("1\n2,3"));    
-console.log(add("1,2"));       
-console.log(add("//;\n1;2"));
+
+
+//Test cases
+console.log(add("//;\n1;2"));    
+console.log(add("1\n2,3"));      
+console.log(add("1"));    
+console.log(add(""));            
+try {
+  console.log(add("//;\n1;-2;-3")); 
+} catch (error) {
+  console.error(error.message);
+}
+
+
+
+
+
+
+
+
+
+
+
