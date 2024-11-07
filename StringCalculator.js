a100af81086f6833f47ad6c84cb8077634e577fa
Subhodip456 <53405787+Subhodip456@users.noreply.github.com>
2024-11-07 20:49:58 +0530

    String calculator with a function add.Input: a string of comma-separated numbers Output: an integer, sum of the numbers
    Signed-off-by: Subhodip456 <53405787+Subhodip456@users.noreply.github.com>

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,43 +1,7 @@
 function add(numbers) {
-    if (!numbers) return 0;
-
-    let delimiter = /,|\n/; 
-    if (numbers.startsWith("//")) {
-      const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
-      if (delimiterMatch) {
-        delimiter = new RegExp(delimiterMatch[1]);
-        numbers = numbers.slice(delimiterMatch[0].length);
-      }
-    }
-    const numbersArray = numbers.split(delimiter);
-    const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0);
-    const negativeNumbers = parsedNumbers.filter(num => num < 0);
-    if (negativeNumbers.length > 0) {
-      throw new Error(`negative numbers not allowed: ${negativeNumbers.join(", ")}`);
-    }
-    return parsedNumbers.reduce((sum, num) => sum + num, 0);
-  }
-
-  
-
-
-console.log(add("//;\n1;2"));    
-console.log(add("1\n2,3"));      
-console.log(add("1"));    
-console.log(add(""));            
-try {
-    console.log(add("//;\n1;-2;-3")); 
-  } catch (error) {
-    console.error(error.message);
+  const numbersArray = numbers.split(","); 
+  const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0); 
+  return parsedNumbers.reduce((sum, num) => sum + num, 0); 
 }
 
-
-
-
-
-
-
-
-
-
-
+console.log(add("1,2")); 
