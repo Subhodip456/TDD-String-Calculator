be23922aa586a81325a06b19366579bc50be6b73
Subhodip456 <53405787+Subhodip456@users.noreply.github.com>
2024-11-07 21:01:08 +0530

    add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)
    Signed-off-by: Subhodip456 <53405787+Subhodip456@users.noreply.github.com>

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,7 +1,8 @@
 function add(numbers) {
-  const numbersArray = numbers.split(","); 
+  const numbersArray = numbers.split(/,|\n/); 
   const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0); 
   return parsedNumbers.reduce((sum, num) => sum + num, 0); 
 }
 
-console.log(add("1,2")); 
+console.log(add("1\n2,3")); 
+console.log(add("1,2"));    
