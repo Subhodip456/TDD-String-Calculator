fe42dd05b5c76d0a5070ff935f8ffe7977ee4842
Subhodip456 <53405787+Subhodip456@users.noreply.github.com>
2024-11-07 21:04:57 +0530

    Added changes that supoorts delimeter in string //[delimiter]\n[numbers…]
    Support different delimiters:
    
    To change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\n[numbers…]". For example, "//;\n1;2" where the delimiter is ";" should return 3.
    
    Signed-off-by: Subhodip456 <53405787+Subhodip456@users.noreply.github.com>

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,8 +1,17 @@
 function add(numbers) {
-  const numbersArray = numbers.split(/,|\n/); 
+  let delimiter = /,|\n/; 
+  if (numbers.startsWith("//")) {
+    const delimiterEnd = numbers.indexOf("\n"); 
+    delimiter = new RegExp(numbers.substring(2, delimiterEnd)); 
+    numbers = numbers.substring(delimiterEnd + 1); 
+  }
+
+  const numbersArray = numbers.split(delimiter); 
   const parsedNumbers = numbersArray.map(num => parseFloat(num) || 0); 
   return parsedNumbers.reduce((sum, num) => sum + num, 0); 
 }
 
-console.log(add("1\n2,3")); 
-console.log(add("1,2"));    
+// Test cases
+console.log(add("1\n2,3"));    
+console.log(add("1,2"));       
+console.log(add("//;\n1;2"));
